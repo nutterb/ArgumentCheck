1f1be4396a616269b3ebb20a78e48e48a245d1c7
Benjamin <benjamin.nutter@gmail.com>
2015-07-09 19:42:12 -0400

    Use .onAttach instead of .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(libname,pkgname)
+.onAttach <- function(libname, pkgname)
 {
   packageStartupMessage(paste0("ArgumentCheck 0.10.0 is not backward compatible with version ",
                                "0.9.0.\nPlease see the NEWS file or the GitHub page for details."))
